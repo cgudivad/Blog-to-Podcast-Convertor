ce09cc89b6bd8c5d2087236069bd6e0b5f5911f5
cgudivad <126507537+cgudivad@users.noreply.github.com>
2023-12-15 19:24:17 -0600

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,4 +1,4 @@
-open_AI_GPT_model = "gpt-3.5-turbo-1106"
-open_AI_API_key = "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"
-elevenlabs_API_key = "0595ee98fecb3af084820be05fbb3afb"
-heygen_API_key = "MzFjYzUwZTdlNTVjNDE2Y2I5MTg2YTZkMGIxZGViNzEtMTY5ODE5ODYwMQ=="
+open_AI_GPT_model = "insert-chat-gpt-model-here"
+open_AI_API_key = "insert-openai-api-key-here"
+elevenlabs_API_key = "insert-elevenlabs-api-key-here"
+heygen_API_key = "insert-heygen-api-key-here"
